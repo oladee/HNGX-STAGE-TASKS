6ca7bf03d15b36f887ccfc83ab2fa92663004fe3
oladee <deemajor230600@gmail.com>
2023-09-08 02:28:45 +0100

    restructured javascript to suit task

--- Frontend page with specific element/index.js
+++ Frontend page with specific element/index.js
@@ -4,6 +4,18 @@ let utc = document.getElementById('utc');
 const arrayOfDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday','Thursday','Friday','Saturday']
 let weekDay = new Date().getDay()
 currentDay.innerText = arrayOfDays[weekDay]
-setInterval(()=>{
-    utc.innerText = new Date().toUTCString()
+
+function validUTCDate(date){
+    const hours = String(date.getUTCHours()).padStart(2,'0');
+    const minutes = String(date.getUTCMinutes()).padStart(2,'0');
+    const seconds = String(date.getUTCSeconds()).padStart(2,'0');
+    const milliseconds = String(date.getUTCMilliseconds()).padStart(3,'0');
+
+    return `${hours}:${minutes}:${seconds}:${milliseconds}`
+}
+
+setInterval(()=> {
+    const currentTime = new Date()
+    
+    utc.innerText = validUTCDate(currentTime)
 },1000)
